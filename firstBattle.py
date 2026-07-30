d0b98a9196280d7d7128e23b9636bad7ca835b11
Kadendotcom <kadenbic@buffalo.edu>
2026-07-30 01:15:49 -0400

    virtual env creation. gitignore added. firstBattle completed

---
+++ firstBattle.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from poke_env.player import RandomPlayer
+
+async def main():
+    player_1= RandomPlayer(max_concurrent_battles=1)
+    player_2= RandomPlayer(max_concurrent_battles=1)
+    
+    await player_1.battle_against(player_2, n_battles=1)
+    
+    print(f"Finished Battles: {player_1.n_finished_battles}")
+    print(f"Player 1 wins: {player_1.n_won_battles}")
+    
+if __name__ == "__main__":
+    asyncio.run(main())
+    
